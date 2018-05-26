3bdbd5951c93e0ba95a2860a0302c61f6eb5ede0
Kaindls <Kaindls@2217PC08860.voestalpine.root.local>
2018-05-26 11:00:46 +0200

    DB Inhalt an Frontend - versuch

---
+++ 2018-SemTech/src/main/java/at/jku/dke/semtech2018/skiwc/WebApplication.java
@@ -0,0 +1,43 @@
+package at.jku.dke.semtech2018.skiwc;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.tdb.TDBFactory;
+import org.apache.jena.vocabulary.RDFS;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("at.jku.dke.semtech2018.web.controller")
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+	@Bean
+	public Dataset rdf() {
+		// create an empty Model
+		// Model m = ModelFactory.createDefaultModel();
+		Dataset dataset = TDBFactory.assembleDataset(
+				SkiWC_updateTDB.class.getResource("skiwc-assembler.ttl").getPath()) ;
+		return dataset;
+
+		// set namespace prefixes
+		// m.setNsPrefix("", "http://example.org/");
+		// m.setNsPrefix("rdfs", RDFS.getURI());
+
+		// create a resource
+		// Resource example = m.createResource("http://example.org/helloworld");
+
+		// add a property
+		//example.addProperty(RDFS.label, "Hello World!");
+		//return m;
+	}
+
+}
--- 2018-SemTech/src/main/java/at/jku/dke/semtech2018/web/controller/GreetingController.java
+++ 2018-SemTech/src/main/java/at/jku/dke/semtech2018/web/controller/GreetingController.java
@@ -1,5 +1,13 @@
 package at.jku.dke.semtech2018.web.controller;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +18,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 	@Autowired
-	protected org.apache.jena.rdf.model.Model m;
+	//protected org.apache.jena.rdf.model.Model m;
+	protected Dataset d;
 	
     @GetMapping("/greeting")
+    //public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-    	model.addAttribute("name2", m.toString());
-        model.addAttribute("name", name);
+    	//model.addAttribute("name2", m.toString());
+        //model.addAttribute("name", name);
+    	String resstr;
+    	d.begin(ReadWrite.READ);  
+		try {
+			Query q = QueryFactory.create("SELECT ?s ?p ?o ?g WHERE {{?s ?p ?o} UNION {GRAPH ?g {?s ?p ?o}}}");
+			try (QueryExecution qEx = QueryExecutionFactory.create(q,d) ) {
+				ResultSet res = qEx.execSelect();
+				ResultSetFormatter.out(System.out, res, q);
+				resstr = ResultSetFormatter.asText(res);
+				model.addAttribute("result", resstr);
+				//INFO Stefan: Ausgabe des  DB-Inhalts auf der Console funkioniert, an greeting.html wird leider nur die erste Zeile übergeben...
+			}
+		} finally { d.end(); }
+
+		d.close();
         return "greeting";
     }
 
